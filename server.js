0fe40aeedc14a0be3e35658a09f2d6750ac22217
LizandroV <61571830+LizandroV@users.noreply.github.com>
2025-03-07 19:26:37 -0500

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Server-side code running');
+//espress web server    
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello Lizandro!');
+    });
+
+app.listen(process.env.port || port);
+console.log(`Server is running on port ${port}`);
